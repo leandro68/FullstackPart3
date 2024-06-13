fa0ac52d09f666bc6c9fe4ed5e788314e335811a
Lean <leandrorodriguez68@gmail.com>
2024-06-13 16:02:18 -0300

    Exercise 3.10 delete fixed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
