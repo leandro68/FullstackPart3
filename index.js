1e633b8a6080ef0277b866f1729a178ad97041e7
Lean <leandrorodriguez68@gmail.com>
2024-06-13 10:24:20 -0300

    Exercise 3.2 solution

--- index.js
+++ index.js
@@ -34,7 +34,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                 <br/>
+                 <p>${Date()}</p>`)
+})
 
 
 const PORT = 3001
