d02957db3606da4343aa20397f0cf627a6c8f64b
Lean <leandrorodriguez68@gmail.com>
2024-06-13 18:41:04 -0300

    uibuild

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ app.delete('/api/persons/:id', ( request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body.name,body.number)  
   if(!body.name || !body.number){
     console.log('name or phone missing')
     return response.status(400).json({error: 'name or phone missing'})
