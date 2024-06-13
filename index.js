f97c2fd32e898c7ecd64f42d069e8753e05b03cc
Lean <leandrorodriguez68@gmail.com>
2024-06-13 10:26:01 -0300

    Exercise 3.3 solution

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.get('/info', (request, response) => {
                  <p>${Date()}</p>`)
 })
 
+app.get('/api/persons/:id', ( request, response ) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
