9b42835a110de79d570effb42ec374e4fd33812f
Lean <leandrorodriguez68@gmail.com>
2024-06-19 10:53:16 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,76 +1,80 @@
-const express = require('express')
-const app = express()
 require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-const Note = require('./models/note')
 
-let notes = [
-]
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
 
 app.use(express.static('dist'))
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const cors = require('cors')
 
-app.use(cors())
 
-app.use(express.json())
-app.use(requestLogger)
+let persons = []
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Welcome to phonebook app</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => response.json(persons))
 })
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                 <br/>
+                 <p>${Date()}</p>`)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', ( request, response ) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
 
+app.delete('/api/persons/:id', ( request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
   response.status(204).end()
 })
 
-app.use(unknownEndpoint)
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  console.log('name:',body.name,typeof(body.name),'number:',body.number)  
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log('Phonebook app listen on port 3001')
 })
