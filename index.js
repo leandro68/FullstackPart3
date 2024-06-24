ab4926c33d1a0e846f9833fa60b5f26972289312
Lean <leandrorodriguez68@gmail.com>
2024-06-24 08:33:57 -0300

    uibuild

--- index.js
+++ index.js
@@ -91,14 +91,9 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number} = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
